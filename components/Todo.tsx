8f0101543c7bfca00671010f77dc1e92fa9db71c
Steven Zimmer <webdevzim@gmail.com>
2023-06-12 15:23:43 -0700

    todo list complete

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,13 +1,63 @@
+"use client";
 
+import { useState, useEffect } from "react";
+
+import { deleteTodo, editTodo } from "@/utils/actions";
 type Props = {
   todo: {
     content: string;
-  }
+    id: number;
+  },
 }
+
 export default function Todo({todo}: Props) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [content, setContent] = useState(todo.content);
+  const handleEdit = (e) => {
+    // console.log("edit");
+    setIsEditing(!isEditing);
+
+  }
+
+  const handleDelete = (id: number) => {
+    // e.preventDefault();
+    deleteTodo(id);
+    // console.log(e.target.value);
+    console.log("delete");
+  }
+
+  const handleSave = (id:number, content:string) => {
+    // console.log("save");
+    editTodo(id, content);
+    setIsEditing(!isEditing);
+  }
+
+
+
   return (
-    <div className="border-2 border-black rounded px-3 py-2 mb-2">
-     <h3 className="text-xl font-bold">{todo.content}</h3>
+    <div className="border-2 border-black rounded px-3 py-2 mb-2 flex items-center justify-between">
+      {isEditing ? (
+        <>
+        <input
+          type="text"
+          className="border-2 border-black rounded px-3 py-2 mb-2 flex items-center justify-between text-black"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <div className="w-8 h-8 text-black rounded-full bg-white flex items-center justify-center cursor-pointer" onClick={() => handleSave(todo.id, content)}>
+          √
+        </div>
+        </>
+      ) : (
+
+       <h3 className="text-xl font-bold">{todo.content}</h3>
+      )}
+     <div>
+        <button onClick={(e) => handleDelete(todo.id)} className="ml-2 bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded">Delete</button>
+
+        <button onClick={(e) => handleEdit(todo.id)} className="ml-2 bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-2 rounded">Edit</button>
+     </div>
     </div>
   )
 }
--- utils/actions.ts
+++ utils/actions.ts
@@ -16,3 +16,32 @@ export const newTodo = async (formData:any ) => {
   console.log({todo});
 
 }
+
+export const deleteTodo = async (id: number) => {
+  console.log("deleteTodo", id);
+  const todo = await prisma.todo.delete({
+    where: {
+      id,
+    },
+  });
+
+  revalidatePath("/todos");
+
+  console.log({todo});
+}
+
+export const editTodo = async (id: number, content: string) => {
+  console.log("editTodo", content);
+  const todo = await prisma.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      content,
+    },
+  });
+
+  revalidatePath("/todos");
+
+  console.log({todo});
+}
